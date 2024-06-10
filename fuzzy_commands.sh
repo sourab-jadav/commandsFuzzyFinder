50424a2e15835d67e9a3213f47fd991a6d428533
sourabh jadav <sourabhjadav7@gmail.com>
2024-06-10 12:14:07 +0530

    add prompt feature

--- fuzzy_commands.sh
+++ fuzzy_commands.sh
@@ -10,17 +10,24 @@ if [[ ! -f $COMMANDS_FILE ]]; then
 fi
 
 # Use fzf to fuzzy find a command from the file
-SELECTED_LINE=$(cat $COMMANDS_FILE | fzf )
+SELECTED_LINE=$(cat $COMMANDS_FILE | fzf)
 
-# If a line was selected, extract and execute the command
+# If a line was selected, extract the command
 if [[ -n "$SELECTED_LINE" ]]; then
     # Extract the command using one or more spaces as the delimiter
     COMMAND=$(echo "$SELECTED_LINE" | awk -F' + ' '{print $1}')
     
-    # Execute the command
-    echo "Executing: $COMMAND"
-    echo "$COMMAND" | xclip -selection clipboard
-    eval "$COMMAND"
-fi
+    # Prompt the user to either execute or copy the command
+    read -p "Do you want to execute the command? (y/n): " choice
 
+    if [[ $choice == "y" ]]; then
+        # Execute the command
+        echo "Executing: $COMMAND"
+        eval "$COMMAND"
+    else
+        # Copy the command to the clipboard
+        echo "Copying the command to clipboard."
+        echo "$COMMAND" | xclip -selection clipboard
+    fi
+fi
 
